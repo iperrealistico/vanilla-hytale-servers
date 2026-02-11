dd4747d9078765bf55448acd54d6d469a5cc261f
iperrealistico <iperrealistico@users.noreply.github.com>
2026-02-11 18:22:09 +0100

    Add and configure favicon in multiple sizes

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,9 +35,10 @@ export default function RootLayout({
           }}
         />
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" href="/img/favicon.png" />
-        <link rel="apple-touch-icon" href="/img/favicon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <link rel="stylesheet" href="/vendor/fonts.css" />
         <link rel="stylesheet" href="/vendor/font-awesome.min.css" />
       </head>
